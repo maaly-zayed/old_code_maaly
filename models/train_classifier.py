8dc2bb47b59c574fbc423437debc3f62091613f6
maaly <m.algharaibeh@t2.sa>
2023-06-27 10:59:55 +0300

    update comments in train classifier

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -18,104 +18,106 @@ from sklearn.multioutput import MultiOutputClassifier
 import pickle
 from sklearn.metrics import precision_score
 
+
 def load_data(database_filepath):
     """
     The function to load the created database.
+
     Parameters:
     database_filepath: the file path to the database_filepath.
     """
-    # create a connection to the sql database
+    # Create a connection to the sql database
     engine = create_engine('sqlite:///'+database_filepath)
-    #read sql tables into datafram
+    # Read sql tables into datafram
     df = pd.read_sql_table('DisasterResponse', engine)
 
-    # get the message data
+    # Get the message data
     message = df['message']
-    # get the categories name
+    # Get the categories name
     categories = df.iloc[:,4:]
     category_names = categories.columns 
+
     return message, categories, category_names
+
 
 def tokenize(text):
     """
     The function to tokenize the text messages.
+
     Parameters:
     text: text messages.
     """
-    # preprocessing using regex to detect url in the message
+    # Preprocessing using regex to detect url in the message
     url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-    # find all url in the message text
+    # Find all url in the message text
     detected_urls = re.findall(url_regex, text)
-    # replace all url with "urlplaceholder"
+    # Replace all url with "urlplaceholder"
     for url in detected_urls:
         text = text.replace(url, "urlplaceholder")
 
-     # convert text messages into tokens
+     # Convert text messages into tokens
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
-    # preprocessing
+    # Preprocessing
     lemm_lower_tokens = []
     for token in tokens:
-        #lemmatize each token 
+        # Lemmatize each token 
         process_token = lemmatizer.lemmatize(token).lower().strip()
         lemm_lower_tokens.append(process_token)
+
     return lemm_lower_tokens    
 
 
-
 def build_model():
-    
     """
     The function to create a pipeline for this model.
+
     Output: the function return the model
     """
-    #create a pipline of CountVectorizer, TfidfTransformer and MultiOutputClassifier
+    # Create a pipline of CountVectorizer, TfidfTransformer and MultiOutputClassifier
     pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer=tokenize)),
         ('tfidf', TfidfTransformer()),
         ('clf', MultiOutputClassifier(RandomForestClassifier()))
-    ])
-    #set the parameters 
+        ])
+    # Set the parameters 
     # parameters = {
     #     'clf__estimator__n_estimators': [10,20],
-    # 'clf__estimator__min_samples_split': [2,4],
-    
+    #     'clf__estimator__min_samples_split': [2,4],
     # }
-    #Note i change the parameters because I couldn't push the model to github
+    # Note i change the parameters because I couldn't push the model to github
     parameters = {
         'clf__estimator__n_estimators': [10],
-    'clf__estimator__min_samples_split': [2,4],
-    
-    }
+        'clf__estimator__min_samples_split': [2,4],
+        }
     model = GridSearchCV(pipeline, param_grid=parameters, n_jobs=4, verbose=2, cv=3)
+
     return model
-
-
-
 
 
 def evaluate_model(model, X_test, y_test, category_names):
     """
     The function to evaluate the model performance.
+
     Parameters:
     model: trained model.
     X_test: test text message
     y_test: the correct target
     category_names: the target names
+
     output: priniting the classification report
     """
-    # get the prediction of test data
+    # Get the prediction of test data
     y_pred = model.predict(X_test)
     for indx, category in enumerate(category_names):
-        print(f'_____________{indx, category} ______________')
-        print('________________________________________________')
+        print('The classification_report for: {}'.format(category))
         print(classification_report(list(y_test.values[:, indx]), list(y_pred[:, indx])))
-        
 
 
 def save_model(model, model_filepath):
     """
     The function to save model.
+
     Parameters:
     model: trained model.
     model_filepath: the model path
@@ -131,11 +133,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, y, category_names = load_data(database_filepath)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
         print('Building model...')
         model = build_model()
 
         print('Training model...')
         model.fit(X_train, y_train)
+
         print('Evaluating model...')
         evaluate_model(model, X_test, y_test, category_names)
 
